cb3e849e5e13f25ed7c0e04c0655383d7d3d4d41
liamcomerweaver <78572114+liamcomerweaver@users.noreply.github.com>
2024-03-20 07:54:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ import gunicorn
 
 # A. Load the data 
 # Preprocessed CSV from MM_Cleaner must be in the same directory as this notebook file
-MM = pd.read_csv("MM_Dummies19.csv")
+MM = pd.read_csv("MM_Dummies20.csv")
 
 
 # In[6]:
